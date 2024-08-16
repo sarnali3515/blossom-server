3a9885b65ca6bc5e94e65d559f505f77835dc398
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-08-16 17:08:37 +0600

    products get api added

--- index.js
+++ index.js
@@ -27,12 +27,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const productCollection = client.db("blossom").collection("products")
+
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close()
     }
 }
 run().catch(console.dir);
